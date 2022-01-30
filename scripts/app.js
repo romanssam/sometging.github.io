f40f0ce07cf9a77f019c4f13676ab1a55e631b78
romanssam <troubledyouthh@outlook.com>
2022-01-30 21:23:12 +0500

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+const bvi = {
+  BviPanel: "1",
+  BviPanelBg: "white",
+  BviPanelFontSize: "18",
+  BviPanelLetterSpacing: "normal",
+  BviPanelLineHeight: "normal",
+  BviPanelImg: "0",
+  BviPanelImgXY: "0",
+  BviPanelReload: "0",
+  BviPanelNoWork: "0",
+  BviPanelText:
+    "\u0412\u0435\u0440\u0441\u0438\u044f \u0434\u043b\u044f \u0441\u043b\u0430\u0431\u043e\u0432\u0438\u0434\u044f\u0449\u0438\u0445",
+  BviPanelCloseText:
+    "\u041e\u0431\u044b\u0447\u043d\u0430\u044f \u0432\u0435\u0440\u0441\u0438\u044f \u0441\u0430\u0439\u0442\u0430",
+  BviFixPanel: "0",
+  BviCloseClassAndId: "",
+  BviTextBg: "#e53935",
+  BviTextColor: "#ffffff",
+  BviSizeText: "14",
+  BviPlay: "0",
+  BviPanelPlayOn: "0",
+};
